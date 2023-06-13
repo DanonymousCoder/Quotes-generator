cfdb0454a0e5ae0bd6f6ba49af9dff85a7f066bf
Ridwan <aladebaruwa@gmail.com>
2023-06-13 18:42:01 +0100

    py

--- quotes.py
+++ quotes.py
@@ -4,3 +4,5 @@ quotes = [
      "The only way to do great work is to love what you do. - Steve Jobs",
      "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer",
      "Believe you can and you're halfway there. - Theodore Roosevelt",
+     "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+     "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
