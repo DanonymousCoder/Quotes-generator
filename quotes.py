96ad3cc648524c1a7970226f364fa7b87408050b
Ridwan <aladebaruwa@gmail.com>
2023-06-13 18:38:36 +0100

    py

--- quotes.py
+++ quotes.py
@@ -2,4 +2,4 @@ import random
 def get_random_quote():
 quotes = [
      "The only way to do great work is to love what you do. - Steve Jobs",
-     
+     "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer",
