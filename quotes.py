f5d2357044d5b5c9f9f2078aa10a2f26b545cd72
Ridwan <aladebaruwa@gmail.com>
2023-06-13 18:39:12 +0100

    py

--- quotes.py
+++ quotes.py
@@ -3,4 +3,4 @@ def get_random_quote():
 quotes = [
      "The only way to do great work is to love what you do. - Steve Jobs",
      "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer",
- "Believe you can and you're halfway there. - Theodore Roosevelt",
+     "Believe you can and you're halfway there. - Theodore Roosevelt",
